f429203054d0045b341226201f472b8e40fa7439
nuysanbar <nuysanbarorodev@gmail.com>
2022-10-13 10:09:36 +0300

    my first release of QR reader

---
+++ src/qrCodeScanner.js
@@ -0,0 +1,60 @@
+const qrCode = window.qrcode;
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
+
+let scanning = false;
+
+btnScanQR.onclick = () =>{
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
+};
+
+function tick() {
+    canvasElement.height = video.videoHeight;
+    canvasElement.width = video.videoWidth;
+    canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+  
+    scanning && requestAnimationFrame(tick);
+  }
+
+function scan() {
+    try {
+         qrCode.decode();
+    } catch (e) {
+        setTimeout(scan, 300);
+    }
+}
+
+qrCode.callback = (res) => {
+    if (res) {
+      outputData.innerText = res;
+      scanning = false;
+      video.srcObject.getTracks().forEach(track => {
+        track.stop();
+      });
+  
+      qrResult.hidden = false;
+      btnScanQR.hidden = false;
+      canvasElement.hidden = true;
+    }
+  };
+
+
+  
